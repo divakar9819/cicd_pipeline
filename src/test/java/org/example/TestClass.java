2d77d880b14aa0a6f6ef4cf0ad5d4281d8d4607f
divakar9819 <vermadivakar10051998@gmail.com>
2024-05-22 16:24:57 +0530

    initial code

---
+++ src/test/java/org/example/TestClass.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Divakar Verma
+ * @created_at : 02/04/2024 - 3:47 pm
+ * @mail_to: vermadivakar2022@gmail.com
+ */
+public class TestClass {
+
+    @Test
+    public void testMethod(){
+        System.out.println("***********************************************************");
+        System.out.println("Hello Word");
+        System.out.println("******************************************************");
+    }
+}
